00c61831e7bc4677d069fc54e14584b564f55c08
Anton_Sidorov <Anton_sid1984@mail.ru>
2023-10-05 09:13:09 +0300

    Решение задачи с данными пользователя

---
+++ Metod_2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите расстояние");
+int distance = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите скорость первого друга");
+int firstFriendSpeed = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите скорость второго друга");
+int secondFriendSpeed =  int.Parse(Console.ReadLine());
+Console.WriteLine("Введите скорость собаки");
+int dogSpeed = int.Parse(Console.ReadLine());
+int friends = 2;
+int count = 0;
+int time = 0;
+
+if (distance < 10) 
+    {
+    Console.WriteLine("Собака пробежит один раз");
+    } 
+else
+    {while (distance>10)
+    {
+        if(friends == 1)
+        {
+           time = distance/(firstFriendSpeed + dogSpeed);
+           friends = 2; 
+        }
+        else
+        { time = distance/(secondFriendSpeed + dogSpeed);
+           friends = 1; 
+        }
+        distance = distance - (firstFriendSpeed+secondFriendSpeed)*time;
+        count++;
+    }
+    Console.WriteLine("Собака пробежит "+ count+" раз");
+    }
