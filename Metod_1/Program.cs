c71690dc97b15bffd476032089a0b2a27467ddea
Anton_Sidorov <Anton_sid1984@mail.ru>
2023-10-05 09:06:21 +0300

    Решение задачи с заданными параметрами

---
+++ Metod_1/Program.cs
@@ -0,0 +1,28 @@
+﻿int distance = 10000;
+int firstFriendSpeed = 1, secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friends = 2;
+int count = 0;
+int time = 0;
+
+if (distance < 10) 
+    {
+    Console.WriteLine("Собака пробежит один раз");
+    } 
+else
+    {while (distance>10)
+    {
+        if(friends == 1)
+        {
+           time = distance/(firstFriendSpeed + dogSpeed);
+           friends = 2; 
+        }
+        else
+        { time = distance/(secondFriendSpeed + dogSpeed);
+           friends = 1; 
+        }
+        distance = distance - (firstFriendSpeed+secondFriendSpeed)*time;
+        count++;
+    }
+    Console.WriteLine("Собака пробежит "+ count+" раз");
+    }
